8bfcf665dc5c26a36cf084d0fadb05bbf65d8170
Felipe Maccheri <felipemaccheri@gmail.com>
2020-09-27 17:03:09 -0300

    Creating product list layout

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,116 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { MdAddShoppingCart } from 'react-icons/md';
+import { ProductList } from './styles';
 
 function Home() {
-  return <div />;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-pulseboost-hd-adidas-masculino/34/NQQ-5284-234/NQQ-5284-234_zoom1.jpg?ims=326x"
+          alt="Tênis"
+        />
+
+        <strong>
+          Tênis muito legal com título muito grande que qubra várias linhas
+        </strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-pulseboost-hd-adidas-masculino/34/NQQ-5284-234/NQQ-5284-234_zoom1.jpg?ims=326x"
+          alt="Tênis"
+        />
+
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-pulseboost-hd-adidas-masculino/34/NQQ-5284-234/NQQ-5284-234_zoom1.jpg?ims=326x"
+          alt="Tênis"
+        />
+
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-pulseboost-hd-adidas-masculino/34/NQQ-5284-234/NQQ-5284-234_zoom1.jpg?ims=326x"
+          alt="Tênis"
+        />
+
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-pulseboost-hd-adidas-masculino/34/NQQ-5284-234/NQQ-5284-234_zoom1.jpg?ims=326x"
+          alt="Tênis"
+        />
+
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-pulseboost-hd-adidas-masculino/34/NQQ-5284-234/NQQ-5284-234_zoom1.jpg?ims=326x"
+          alt="Tênis"
+        />
+
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
 
 export default Home;
